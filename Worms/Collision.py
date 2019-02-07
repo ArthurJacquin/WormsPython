02dc4664255710791305d4c408ad63bbbee7ef50
Margot <gotgot07@hotmail.fr>
2019-02-07 22:13:22 +0100

    Retouche collision, ajout début rebond

--- Worms/Collision.py
+++ Worms/Collision.py
@@ -2,19 +2,28 @@ import pygame
 from Worms.Physics import *
 from Worms.Rocket import *
 from Worms.Grenade import *
+from Worms.GroundGenerator import *
+from Worms.main import *
 
 #récupération de la position de la balle
-def ImpactPointRocket(rocket):
-    impactPos = pygame.math.Vector2(rocket.x + (rocket.radius/2), rocket.y + (rocket.radius/2))
-    #+ajout du vecteur de la trajectoire pour avoir le point qui suit la trajectoire
+def GroundCollisionRocket(rocket, sol):
+    pos = (rocket.x, rocket.y)
+    for i in sol:
+        if pos == sol[i]:
+            impactPos = pygame.math.Vector2(rocket.x + (rocket.radius/2), rocket.y + (rocket.radius/2))
+            break
     return impactPos
 
-def ImpactPointGrenade(grenade):
-    impactPos = pygame.math.Vector2(grenade.x + (grenade.radius/2), grenade.y + (grenade.radius/2))
+def GroundCollisionGrenade(grenade, sol):
+    pos = (rocket.x, rocket.y)
+    for i in sol :
+        if pos == sol[i]:
+            impactPos = pygame.math.Vector2(grenade.x + (grenade.radius/2), grenade.y + (grenade.radius/2))
+            break
     return impactPos
 
 #Calcul normal sol pour rebonds
-def GroundNormal(impactPos):
+def GroundNormal(impactPos, sol):
     pointA = impactPos + 1
     pointB = pygame.math.Vector2(impactPos.x - 1, impactPos.y)
     pointC = impactPos - 1
@@ -24,15 +33,14 @@ def GroundNormal(impactPos):
 
     #pour chaque point dans le terrain, on calcule la moyenne des vecteurs des points
     for point in tabPoint :
-        if point in #terrain:
-            normal = normal + tabPoint[point]
+        if point in sol:
+            normalPoint = normalPoint + tabPoint[point]
+
+    normal = pygame.math.Vector2(impactPos, normalPoint)
     return normal
 
-#if we define dx=x2-x1 and dy=y2-y1, then the normals are (-dy, dx) and (dy, -dx).
-#normal = pygame.math.Vector2(normal, point d'impact)
-
 #Collision avec le sol
-def GroundCollision(impactPos, normal, rocket, grenade): #index des points du terrain):
-    for i in #terrain :
-        if i == impactPos:
-
+def Rebond(impactPos, grenade, sol):
+    rebonds = 0
+    while(rebonds != 2):
+        #calcul du rebond
